257d3a19bbb314649e7315b5efd01800134a611c
Nicolas Manichon <nmanichon@gmail.com>
2017-11-14 11:40:59 +0100

    Updated the makefile and the filesystem

--- headers/imgprocessing/pixop.h
+++
@@ -1,9 +0,0 @@
-#ifndef PIXOP_H
-#define PIXOP_H
-
-#include <SDL/SDL.h>
-
-Uint32 getpixel(SDL_Surface *surface, unsigned x, unsigned y);
-void putpixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel);
-
-#endif
