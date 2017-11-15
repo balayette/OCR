e32d598629953a372984907a2d6c09e1159bb290
Nicolas Manichon <nicolas.manichon@epita.fr>
2017-11-16 00:55:12 +0100

    Working on the documentation

--- include/neuralnet/save.h
+++ include/neuralnet/save.h
@@ -1,11 +1,45 @@
+/**
+ * \file save.h
+ * \brief header of save.c
+ */
 #ifndef SAVE_NN_H
 #define SAVE_NN_H
 
 #include "neuralnet.h"
 #include <stdio.h>
 
+/*
+  Format of the save file :
+
+  input_count | output_count | hidden_layer_count | neuron_count
+  int            int            int                 int
+
+  for i in range(hidden_layer_count):
+
+  k = hidden_layer[i]->neuron_count
+  k * bias_weight   |     k * hidden_layer[i]->prev_layer_size weight    |
+  k * double          k * hidden_layer[i]->prev_layer_size * double
+
+  endfor
+
+  k = output_count
+  k * bias_weight     |     k * output_layer->prev_layer_size weight      |
+  k * double            k * output_layer->prev_layer_size * double
+
+*/
+
+/**
+ * \brief Save the neural network to a file
+ * \param nn Pointer to a neural network
+ * \param path Path to the save file
+ */
 void save_nn(struct neural_net *nn, char *path);
 
+/**
+ * \brief Load a neural network from a file
+ * \param path Path to the file
+ * \return A pointer to a struct neural_net
+ */
 struct neural_net *load_nn(char *path);
 
 #endif
--- src/neuralnet/save.c
+++ src/neuralnet/save.c
@@ -1,3 +1,7 @@
+/**
+ * \file save.c
+ * \brief Implementation of save.h
+ */
 #include "neuralnet/save.h"
 #include "misc/utils.h"
 #include "neuralnet/layer.h"
@@ -5,25 +9,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-  Format of the save file :
-
-  input_count | output_count | hidden_layer_count | neuron_count
-      int            int            int                 int
-
-  for i in range(hidden_layer_count):
-
-    k = hidden_layer[i]->neuron_count
-    k * bias_weight   |     k * hidden_layer[i]->prev_layer_size weight    |
-       k * double          k * hidden_layer[i]->prev_layer_size * double
-
-  endfor
-
-  k = output_count
-  k * bias_weight     |     k * output_layer->prev_layer_size weight      |
-     k * double            k * output_layer->prev_layer_size * double
-
- */
 
 void save_nn(struct neural_net *nn, char *path) {
     FILE *file = fopen(path, "wb");
@@ -52,59 +37,70 @@ void save_nn(struct neural_net *nn, char *path) {
     }
 
     struct layer *output = nn->layers[nn->hidden_layer_count + 1];
-    fwrite(output->bias_weights, sizeof(double),
-           nn->output_count, file);
-    for(int i = 0; i < output->neuron_count; i++){
-        fwrite(output->weights[i], sizeof(double), output->prev_layer_size, file);
+    fwrite(output->bias_weights, sizeof(double), nn->output_count, file);
+    for (int i = 0; i < output->neuron_count; i++) {
+        fwrite(output->weights[i], sizeof(double), output->prev_layer_size,
+               file);
     }
     fclose(file);
 }
 
-struct neural_net *load_nn(char *path){
+struct neural_net *load_nn(char *path) {
     FILE *file = fopen(path, "rb");
     int input_count, output_count, hidden_layer_count, neuron_count;
 
-    if(!fread(&input_count, sizeof(int), 1, file))
-    {
-        printf("Couldn't read the input count of the neural network from the file\n");
+    if (!fread(&input_count, sizeof(int), 1, file)) {
+        printf("Couldn't read the input count of the neural network from the "
+               "file\n");
         exit(1);
     }
-    if(!fread(&output_count, sizeof(int), 1, file)){
-        printf("Couldn't read the output count of the neural network from the file\n");
+    if (!fread(&output_count, sizeof(int), 1, file)) {
+        printf("Couldn't read the output count of the neural network from the "
+               "file\n");
         exit(1);
     }
-    if(!fread(&hidden_layer_count, sizeof(int), 1, file)){
-        printf("Couldn't read the hidden layer count of the neural network from the file\n");
+    if (!fread(&hidden_layer_count, sizeof(int), 1, file)) {
+        printf("Couldn't read the hidden layer count of the neural network "
+               "from the file\n");
         exit(1);
     }
-    if(!fread(&neuron_count, sizeof(int), 1, file)){
-        printf("Couldn't read the neuron count of the neural network from the file\n");
+    if (!fread(&neuron_count, sizeof(int), 1, file)) {
+        printf("Couldn't read the neuron count of the neural network from the "
+               "file\n");
         exit(1);
     }
 
-    struct neural_net *nn = create_nn(input_count, hidden_layer_count, neuron_count, output_count);
+    struct neural_net *nn =
+        create_nn(input_count, hidden_layer_count, neuron_count, output_count);
 
-    for(int i = 0; i < hidden_layer_count; i++){
-        if(!fread(nn->layers[i + 1]->bias_weights, sizeof(double), nn->layers[i + 1]->neuron_count, file)){
-            printf("Couldn't read the bias weights of the hidden layer %d\n", i);
+    for (int i = 0; i < hidden_layer_count; i++) {
+        if (!fread(nn->layers[i + 1]->bias_weights, sizeof(double),
+                   nn->layers[i + 1]->neuron_count, file)) {
+            printf("Couldn't read the bias weights of the hidden layer %d\n",
+                   i);
             exit(1);
         }
-        for(int k = 0; k < nn->layers[i + 1]->neuron_count; k++){
-            if(!fread(nn->layers[i + 1]->weights[k], sizeof(double), nn->layers[i + 1]->prev_layer_size, file)){
-                printf("Couldn't read the weights for neuron %d of layer %d\n", k, i);
+        for (int k = 0; k < nn->layers[i + 1]->neuron_count; k++) {
+            if (!fread(nn->layers[i + 1]->weights[k], sizeof(double),
+                       nn->layers[i + 1]->prev_layer_size, file)) {
+                printf("Couldn't read the weights for neuron %d of layer %d\n",
+                       k, i);
                 exit(1);
             }
         }
     }
 
     struct layer *output = nn->layers[nn->hidden_layer_count + 1];
-    if(!fread(output->bias_weights, sizeof(double), output->neuron_count, file)){
+    if (!fread(output->bias_weights, sizeof(double), output->neuron_count,
+               file)) {
         printf("Couldn't read the bias weights of the output layer\n");
         exit(1);
     }
-    for(int k = 0; k < output->neuron_count; k++){
-        if(!fread(output->weights[k], sizeof(double), output->prev_layer_size, file)){
-            printf("Couldn't read the weights of neuron %d of output layer\n", k);
+    for (int k = 0; k < output->neuron_count; k++) {
+        if (!fread(output->weights[k], sizeof(double), output->prev_layer_size,
+                   file)) {
+            printf("Couldn't read the weights of neuron %d of output layer\n",
+                   k);
             exit(1);
         }
     }
